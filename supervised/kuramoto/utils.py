09e76255fce6779b6c231cd75e4768df83199b20
zyuwei <zhangyuwei.work@gmail.com>
2019-12-16 15:14:28 -0500

    Add a local connectivity...

--- supervised/kuramoto/utils.py
+++ supervised/kuramoto/utils.py
@@ -1,9 +1,12 @@
 import numpy as np
 from tqdm import tqdm
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 plt.ioff()
+
 
 def show(displayer, phase_list, image, mask, coupling, path, name, group_size, img_side):
     colored_mask = (np.expand_dims(np.expand_dims(np.arange(group_size), axis=0), axis=-1) * mask / group_size).sum(1)
@@ -22,30 +25,56 @@ def show(displayer, phase_list, image, mask, coupling, path, name, group_size, i
     plt.colorbar(im)
     plt.savefig(path + '/coupling_' + name)
     plt.close()
-	
+
+
 def sw_connectivity(img_side, num_cn, critic_dist):
     connectivity = np.zeros((img_side ** 2, num_cn))
     for i in tqdm(range(img_side ** 2)):
         count = 0
         for j in range(img_side ** 2):
-	    x_1 = int(i % img_side)
+            x_1 = int(i % img_side)
             y_1 = int(i // img_side)
-	    x_2 = int(j % img_side)
-	    y_2 = int(j // img_side)
-	    dist = np.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
-	    if (dist < critic_dist) and (dist > 0):
-	        connectivity[i, count] = j
-		count += 1
-        while count < num_cn:
-	    connectivity[i, count:] = \
-	    sorted(np.delete(range(img_side ** 2), connectivity[i, ...]),
-            key=lambda k: np.random.random())[:int(num_cn - count)]
-            count += 1
+            x_2 = int(j % img_side)
+            y_2 = int(j // img_side)
+            dist = np.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+            if (dist < critic_dist) and (dist > 0):
+                connectivity[i, count] = j
+                count += 1
+        connectivity[i, count:] = \
+            sorted(np.delete(range(img_side ** 2), connectivity[i, ...]),
+                   key=lambda k: np.random.random())[:int(num_cn - count)]
         change = sorted(range(num_cn), key=lambda k: np.random.random())[:int(num_cn / 2 + 1)]
-	connectivity[i, ...][change] = \
-        sorted(np.delete(range(img_side ** 2), connectivity[i, ...]),
-	    key=lambda k: np.random.random())[:int(num_cn / 2 + 1)]
+        connectivity[i, ...][change] = \
+            sorted(np.delete(range(img_side ** 2), connectivity[i, ...]),
+                   key=lambda k: np.random.random())[:int(num_cn / 2 + 1)]
     return connectivity
+
+
+def lc_connectivity(img_side, num_cn, critic_dist):
+    connectivity = np.zeros((img_side ** 2, num_cn))
+    for i in tqdm(range(img_side ** 2)):
+        count = 0
+        for j in range(img_side ** 2):
+            x_1 = int(i % img_side)
+            y_1 = int(i // img_side)
+            x_2 = int(j % img_side)
+            y_2 = int(j // img_side)
+            dist = np.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+            if (dist < critic_dist) and (dist > 0):
+                connectivity[i, count] = j
+                count += 1
+        connectivity[i, count:] = i
+    return connectivity
+
+
+def generate_connectivity(img_side, num_cn, critic_dist, mode):
+    if mode == 'local':
+        return lc_connectivity(img_side, num_cn, critic_dist)
+    elif mode == 'small_world':
+        return sw_connectivity(img_side, num_cn, critic_dist)
+    else:
+        raise ValueError('Connectivity mode not exists')
+
 
 # check valid file function
 def valid_file(file_path):
